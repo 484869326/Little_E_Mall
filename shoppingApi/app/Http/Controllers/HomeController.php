f286f9acef4a6706c20faf5d2947c639687c72af
test <484869326@qq.com>
2024-01-15 18:08:56 +0800

    no message

--- shoppingApi/app/Http/Controllers/HomeController.php
+++ shoppingApi/app/Http/Controllers/HomeController.php
@@ -5,7 +5,7 @@ use App\Total;
 
 class HomeController extends Controller {
     public function multidata(){
-        $data['banner']=array("http://shoppingapi.net:3333/swiper/1.1.png","http://shoppingapi.net:3333/swiper/1.1.1.png","http://shoppingapi.net:3333/swiper/1.1.2.png","http://shoppingapi.net:3333/swiper/1.1.3.png" );
+        $data['banner']=array("http://shoppingapi.net:3333/swiper/1.png","http://shoppingapi.net:3333/swiper/2.png","http://shoppingapi.net:3333/swiper/3.png","http://shoppingapi.net:3333/swiper/4.png" );
         $result["data"]=$data;
         Total::json($result);
     }
