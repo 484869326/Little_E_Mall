1b51ea86a0fc8aedf603ab46846ce5d589049c68
test <484869326@qq.com>
2024-01-17 14:30:14 +0800

    no message

--- shoppingApi/app/Http/Controllers/HomeController.php
+++ shoppingApi/app/Http/Controllers/HomeController.php
@@ -9,9 +9,12 @@ class HomeController extends Controller
 {
     public function multidata()
     {
-        $data['banner']=array("http://192.168.3.105:3333/swiper/1.png","http://192.168.3.105:3333/swiper/2.png","http://192.168.3.105:3333/swiper/3.png","http://192.168.3.105:3333/swiper/4.png" );
-        // $data['banner']=array("http://192.168.3.105:3333/swiper/1.png");
-        $data['recommend'] = Category::where('level', '=', '0')->orderByRaw('RAND()')->take(10)->get();
+        $data['banner']=array("http://192.168.3.105:3333/swiper/1.jpg","http://192.168.3.105:3333/swiper/2.jpg","http://192.168.3.105:3333/swiper/3.jpg","http://192.168.3.105:3333/swiper/4.jpg" );
+        $recommend = Category::where('level', '=', '0')->orderByRaw('RAND()')->take(10)->get();
+        foreach($recommend as  $key =>$model){
+            $model['Cimg']=env('APP_URL') . substr_replace($model["Cimg"], "", 0, 1);
+        }
+        $data['recommend'] =$recommend;
         $result["data"]=$data;
         Total::json($result);
     }
