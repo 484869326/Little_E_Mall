ef65c6b84c05ef15f3044844f71bd8f333bc6700
484869326 <86037050+484869326@users.noreply.github.com>
2024-07-22 23:50:36 +0800

    Delete mallApp directory

--- mallApp/service/category.js
+++
@@ -1,6 +0,0 @@
-import Request1 from "./index.js";
-
-const getCategoryList = () => {
-  return Request1.get("/categoryList");
-};
-export { getCategoryList };
--- mallApp/service/search.js
+++
@@ -1,7 +0,0 @@
-import Request1 from "./index.js";
-
-const getSearchList = (searchValue, page, limit = 10) => {
-  return Request1.get("/search", { searchValue, page, limit });
-};
-
-export { getSearchList };
